7f435e0392762852f23bd6d2b971f21e393e3324
silverwind <me@silverwind.io>
2015-07-12 15:58:34 +0200

    add workaround for captive portals - #3

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
+var dns = require('dns');
 var net = require('net');
 var eachAsync = require('each-async');
 var onetime = require('onetime');
 var arrify = require('arrify');
+var tlds = require('tlds');
+var ip = require('ip');
 
 var port = 80;
 var timeout = 1000;
@@ -11,25 +14,61 @@ module.exports = function (hostnames, cb) {
 	cb = onetime(cb);
 
 	eachAsync(arrify(hostnames), function (hostname, i, done) {
-		isPortReachable(hostname, port, function (reachable) {
-			if (reachable) {
-				cb(null, true);
-				done(new Error()); // skip to end
-			} else {
-				done();
+		lookup(hostname, function (err, address) {
+			// Skip connecting if a dns lookup fails.
+			if (err) {
+				return done();
 			}
+
+			// When a public domains returns a private ip address we declare the host
+			// as unreachable. This will fail when a intranet resource uses a public
+			// top level domain to resolve to an private address, which is a
+			// violation of RFC 1918 (https://www.ietf.org/rfc/rfc1918.txt).
+			if (isPublicDomain(hostname) && ip.isPrivate(address)) {
+				return done();
+			}
+
+			isPortReachable(address, port, function (reachable) {
+				if (reachable) {
+					cb(null, true);
+					done(new Error()); // skip to end
+				} else {
+					done();
+				}
+			});
 		});
 	}, function () {
 		cb(null, false);
 	});
 };
 
+function lookup(hostname, cb) {
+	if (net.isIP(hostname)) {
+		cb(null, hostname);
+	} else {
+		dns.lookup(hostname, function (err, address) {
+			if (err) {
+				return cb(err);
+			}
+			cb(null, address);
+		});
+	}
+}
 
-function isPortReachable(hostname, port, cb) {
+function isPublicDomain(domain) {
+	var parts = domain.split('.');
+	if (parts[1] && tlds.indexOf(parts[1]) !== -1) {
+		return true;
+	}
+	return false;
+}
+
+function isPortReachable(ip, port, cb) {
 	cb = onetime(cb);
 
 	var socket = new net.Socket();
-	var onError = function () {
+
+	function onError () {
 		cb(false);
 		socket.destroy();
 	}
@@ -38,7 +77,7 @@ function isPortReachable(hostname, port, cb) {
 	socket.on('error', onError);
 	socket.on('timeout', onError);
 
-	socket.connect(port, hostname, function () {
+	socket.connect(port, ip, function () {
 		cb(true);
 		socket.end();
 	});
