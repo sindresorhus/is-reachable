f6b24141f1af4ca84b393303aff902c4d350b001
Sindre Sorhus <sindresorhus@gmail.com>
2015-08-29 16:36:06 +0700

    bump `ava`

--- test.js
+++ test.js
@@ -5,15 +5,15 @@ var isReachable = require('./');
 test(function (t) {
 	t.plan(3);
 
-	isReachable('google.com', function (err, reachable) {
-		t.assert(reachable);
+	isReachable('google.com', function (_, reachable) {
+		t.true(reachable);
 	});
 
-	isReachable('google.com:80', function (err, reachable) {
-		t.assert(reachable);
+	isReachable('google.com:80', function (_, reachable) {
+		t.true(reachable);
 	});
 
-	isReachable('343645335341233123125235623452344123.com', function (err, reachable) {
-		t.assert(!reachable);
+	isReachable('343645335341233123125235623452344123.com', function (_, reachable) {
+		t.false(reachable);
 	});
 });
