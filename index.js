346e957e33be4f2ecf94e504ec11368ab24736c7
silverwind <me@silverwind.io>
2015-12-21 18:53:36 +0100

    add handling for dns intercept and http redirect - #3

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 'use strict';
 var dns = require('dns');
-var eachAsync = require('each-async');
-var onetime = require('onetime');
 var arrify = require('arrify');
+var eachAsync = require('each-async');
 var isPortReachable = require('is-port-reachable');
+var onetime = require('onetime');
+var routerIps = require('router-ips');
+var url = require('url');
 var urlParseLax = require('url-parse-lax');
 
-module.exports = function (hosts, cb) {
+module.exports = function (dests, cb) {
 	cb = onetime(cb);
 
-	eachAsync(arrify(hosts), function (host, i, done) {
-		host = urlParseLax(host);
+	eachAsync(arrify(dests), function (dest, i, done) {
+		dest = urlParseLax(dest);
 
-		dns.lookup(host.hostname, function (_, address) {
+		var host = dest.hostname;
+		var port = dest.port || 80;
+
+		dns.lookup(host, function (_, address) {
 			// Ignore `err` as we only care about `address`.
 			// Skip connecting if there is nothing to connect to.
 			if (!address) {
@@ -20,16 +25,43 @@ module.exports = function (hosts, cb) {
 				return;
 			}
 
-			isPortReachable(host.port || 80, {host: address}, function (_, reachable) {
-				if (reachable) {
-					cb(null, true);
+			// When the returned address is a well-known router ip, we might
+			// have been redirected to a router's captive portal.
+			if (routerIps.indexOf(address) !== -1) {
+				done();
+				return;
+			}
 
-					// skip to end
-					done(new Error());
-				} else {
+			if (port === 80 || port === 443) {
+				// Try to detect HTTP redirection by checking if the `Location`
+				// header contains a well-known router ip.
+				// https://github.com/sindresorhus/is-reachable/issues/3#issuecomment-138735338
+				require(port === 80 ? 'http' : 'https').get({host: host}, function (res) {
+					var redirectHost = url.parse(res.headers.location).host;
+					if (routerIps.indexOf(redirectHost) === -1) {
+						cb(null, true);
+
+						// skip to end
+						done(new Error());
+					} else {
+						done();
+					}
+				}).on('error', function () {
 					done();
-				}
-			});
+				});
+			} else {
+				// Just test if the port answers to a SYN
+				isPortReachable(port, {host: address}, function (_, reachable) {
+					if (reachable) {
+						cb(null, true);
+
+						// skip to end
+						done(new Error());
+					} else {
+						done();
+					}
+				});
+			}
 		});
 	}, function () {
 		cb(null, false);
