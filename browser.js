91e783dc0241cc8afb50f7094e3f6b4f34aa05bd
Daniel Mahon <daniel@mahonstudios.com>
2017-01-14 14:58:32 -0500

    Adds browser support for defining optional protocol and port

--- browser.js
+++ browser.js
@@ -2,16 +2,23 @@
 'use strict';
 const arrify = require('arrify');
 const pAny = require('p-any');
+const urlParseLax = require('url-parse-lax');
 
 module.exports = hosts => {
 	return pAny(arrify(hosts).map(x => {
 		return new Promise(resolve => {
 			const img = new Image();
 
+			x = urlParseLax(x);
+
+			const protocol = x.protocol || '';
+			const host = x.hostname;
+			const port = x.port ? `:${x.port}` : '';
+
 			img.onload = () => resolve(true);
 			img.onerror = () => resolve(false);
 
-			img.src = `//${x}/favicon.ico?${Date.now()}`;
+			img.src = `${protocol}//${host}${port}/favicon.ico?${Date.now()}`;
 		});
 	}));
 };
