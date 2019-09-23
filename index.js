288fc10232525b5100f3a3363ab069f68a2e8201
silverwind <me@silverwind.io>
2019-09-23 08:17:59 +0200

    Remove support for well-known protocols (#38)

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const arrify = require('arrify');
 const got = require('got');
 const isPortReachable = require('is-port-reachable');
 const pAny = require('p-any');
-const portNumbers = require('port-numbers');
 const pTimeout = require('p-timeout');
 const prependHttp = require('prepend-http');
 const routerIps = require('router-ips');
@@ -14,27 +13,30 @@ const URL = require('url-parse');
 
 const dnsLookupP = promisify(dns.lookup);
 
-const checkRedirection = async target => {
+const checkHttp = async url => {
 	let response;
 	try {
-		response = await got(target, {rejectUnauthorized: false});
+		response = await got(url, {rejectUnauthorized: false});
 	} catch (_) {
 		return false;
 	}
 
-	const url = new URL(response.headers.location || 'x://x');
-	return !routerIps.has(url.hostname);
+	if (response.headers && response.headers.location) {
+		const url = new URL(response.headers.location);
+		const hostname = url.hostname.replace(/^\[/, '').replace(/\]$/, ''); // Strip [] from IPv6
+		return !routerIps.has(hostname);
+	}
+
+	return true;
 };
 
 const getAddress = async hostname => net.isIP(hostname) ? hostname : (await dnsLookupP(hostname)).address;
 
 const isTargetReachable = async target => {
 	const url = new URL(prependHttp(target));
-	url.port = Number(url.port) || portNumbers.getPort(url.protocol.slice(0, -1)).port || 80;
 
-	if (!/^[a-z]+:\/\//.test(target)) {
-		const service = portNumbers.getService(url.port);
-		url.protocol = ((service && service.name) ? service.name : 'unknown') + ':';
+	if (!url.port) {
+		url.port = url.protocol === 'http:' ? 80 : 443;
 	}
 
 	let address;
@@ -48,8 +50,8 @@ const isTargetReachable = async target => {
 		return false;
 	}
 
-	if (url.protocol === 'http:' || url.protocol === 'https:') {
-		return checkRedirection(url.toString());
+	if ([80, 443].includes(url.port)) {
+		return checkHttp(url.toString());
 	}
 
 	return isPortReachable(url.port, {host: address});
